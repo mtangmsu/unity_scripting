0146e15dd309815284e05816485f57b06c5a5289
Ming Tang <tangmg@ucmail.uc.edu>
2017-04-15 10:46:56 -0400

    Add files via upload

---
+++ togglelight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class togglelight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            //GetComponent<Light>().GetComponent<Light>().enabled = false;
+//Light myLight = GetComponent<Light>().GetComponent<Light>();
+            Light myLight = GetComponent<Light>();
+            myLight.enabled = !myLight.enabled;
+        }
+
+        }
+}
