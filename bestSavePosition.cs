7a52af7e185d88b67ccc889c887cf5648016c27f
Ming Tang <tangmg@ucmail.uc.edu>
2017-04-17 11:25:10 -0400

    Add files via upload

---
+++ bestSavePosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class bestSavePosition : MonoBehaviour
+{
+    public float samplingRate = 1f; // sample rate in Hz
+    public string outputFilePath;
+
+    private StreamWriter _sw;
+
+    public void OnEnable()
+    {
+        _sw = System.IO.File.AppendText(outputFilePath);
+        InvokeRepeating("SampleNow", 0, 1 / samplingRate);
+    }
+
+    public void OnDisable()
+    {
+        _sw.Close();
+        CancelInvoke();
+    }
+
+    public void SampleNow()
+    {
+        //_sw.WriteLine("t {0} x {1} z {2} fx {3} fz {4}",
+        _sw.WriteLine("t {0} x {1} z {2} y {3}",
+           //Time.time, transform.position.x, transform.position.z, transform.forward.x, transform.forward.z);
+           Time.time, transform.position.x, transform.position.z, transform.position.y);
+    }
+}
+
+
